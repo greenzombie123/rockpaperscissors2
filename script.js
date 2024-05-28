7d38d76946e47ca65ace9f26716dec7b204287f9
Alvin Hart <105436283+greenzombie123@users.noreply.github.com>
2024-05-28 11:29:39 +0900

    Create function that randomly gets a string rock paper or scissors

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("Hello!");
+var playerChoice;
+var computerChoice;
+function playGame(choice) {
+    getComputerChoice();
+}
+function getComputerChoice() {
+    var choices = ["rock", "paper", "scissors"];
+    var index = Math.floor(Math.random() * 3);
+    return choices[index];
+}
--- script.ts
+++ script.ts
@@ -1,1 +1,13 @@
-console.log("Hello!")
+let playerChoice;
+let computerChoice;
+
+function playGame(choice:string){
+}
+
+function getComputerChoice(){
+    const choices = ["rock", "paper", "scissors"];
+
+    let index = Math.floor(Math.random() * 3)
+
+    return choices[index]
+}
