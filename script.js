0e4521fd168074e9bed1b810ddae028c572b0f5c
Alvin Hart <105436283+greenzombie123@users.noreply.github.com>
2024-05-28 12:38:37 +0900

    Make checkWinner function that tells the winner of the round

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 var playerChoice;
 var computerChoice;
+var playerScore = 0;
+var computerScore = 0;
 function playGame(choice) {
 }
 function getComputerChoice() {
     var choices = ["rock", "paper", "scissors"];
     var index = Math.floor(Math.random() * 3);
     return choices[index];
+}
+function checkWinner(playerChoice, computerChoice) {
+    if (playerChoice === "rock" && computerChoice === "scissors" ||
+        playerChoice === "scissors" && computerChoice === "paper" ||
+        playerChoice === "paper" && computerChoice === "rock") {
+        return "player";
+    }
+    else if (playerChoice === "rock" && computerChoice === "paper" ||
+        playerChoice === "scissors" && computerChoice === "rock" ||
+        playerChoice === "paper" && computerChoice === "scissors") {
+        return "computer";
+    }
+    else
+        return "draw";
 }
--- script.ts
+++ script.ts
@@ -1,15 +1,31 @@
-let playerChoice: "rock"|"paper"|"scissors";
-let computerChoice:"rock"|"paper"|"scissors";
+let playerChoice: "rock" | "paper" | "scissors";
+let computerChoice: "rock" | "paper" | "scissors";
 let playerScore = 0;
 let computerScore = 0;
 
-function playGame(choice:string){
+function playGame(choice: string) {
 }
 
-function getComputerChoice():string{
+function getComputerChoice(): string {
     const choices = ["rock", "paper", "scissors"];
 
     let index = Math.floor(Math.random() * 3)
 
     return choices[index]
+}
+
+function checkWinner(playerChoice: string, computerChoice: string): string {
+    if (playerChoice === "rock" && computerChoice === "scissors" ||
+        playerChoice === "scissors" && computerChoice === "paper" ||
+        playerChoice === "paper" && computerChoice === "rock"
+    ) {
+        return "player"
+    }
+    else if (playerChoice === "rock" && computerChoice === "paper" ||
+    playerChoice === "scissors" && computerChoice === "rock" ||
+    playerChoice === "paper" && computerChoice === "scissors"){
+        return "computer"
+    }
+    else 
+        return "draw"
 }
